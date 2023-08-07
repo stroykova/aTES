e76eea9aad4988adeade8b223653e764a1d1ced0
Ksenia Stroykova <k.a.stroykova@gmail.com>
2023-08-07 19:29:25 +0000

    not secure oauth2

--- services/sso/init_db.py
+++ services/sso/init_db.py
@@ -6,13 +6,38 @@ def table_exists(table_name):
     res = cur.execute(f"SELECT name FROM sqlite_master WHERE name='{table_name}'")
     return res.fetchone() is not None
 
+
+def get_user(username: str):
+    cur = con.cursor()
+    res = cur.execute(f"SELECT username, hashed_password FROM users WHERE username='{username}'")
+    return res.fetchone()
+    
+
 if __name__ == '__main__':
     
     cur = con.cursor()
+    res = cur.execute(f"SELECT * FROM users")
+    print(res.fetchall())
+
+
+    cur = con.cursor()
 
     table_name = 'users'
-    fields = ('email', 'hashed_password')
+    fields = ('username', 'hashed_password')
     if not table_exists(table_name):
         statement = f"CREATE TABLE {table_name}({', '.join(fields)})"
         print(statement)
         cur.execute(statement)
+
+    if not get_user('johndoe'):
+        statement = f"insert into {table_name} values ('johndoe', 'fakehashedsecret')"
+        cur.execute(statement)
+
+    cur = con.cursor()
+    res = cur.execute(f"SELECT * FROM users")
+    print(res.fetchall())
+
+    res = cur.execute("SELECT username, hashed_password FROM users WHERE username='johndoe'")
+    result = res.fetchone()
+    print(result)
+    con.commit()  
--- services/sso/main.py
+++ services/sso/main.py
@@ -1,6 +1,8 @@
 import sqlite3
+from typing import Annotated
 from fastapi import FastAPI
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi import Depends, FastAPI, HTTPException, status
 from pydantic import BaseModel
 
 SERVICE = 'sso'
@@ -18,21 +20,61 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
 class User(BaseModel):
-    email: str
+    username: str
 
 
 class UserInDB(User):
     hashed_password: str
 
 
-def get_user(email: str):
+def get_user(username: str):
     cur = con.cursor()
-    res = cur.execute(f"SELECT email, hashed_password FROM users WHERE email='{email}'")
+    res = cur.execute(f"SELECT * FROM users")
+    print(res.fetchall())
+
+    cur = con.cursor()
+    res = cur.execute(f"SELECT username, hashed_password FROM users WHERE username='{username}'")
     result = res.fetchone()
+    print(result)
     if result:
-        return UserInDB(email=result[0], hashed_password=result[1])
+        return UserInDB(username=result[0], hashed_password=result[1])
     
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def fake_decode_token(token):
+    # This doesn't provide any security at all
+    # Check the next version
+    user = get_user(token)
+    return user
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    user = fake_decode_token(token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
+
+
+
+@app.post("/token")
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    print(form_data.username)
+    user = get_user(form_data.username)
+    print(user)
+    if not user:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    hashed_password = fake_hash_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    return {"access_token": user.username, "token_type": "bearer"}
+
+
+@app.get("/users/me")
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_user)]
+):
+    return current_user
